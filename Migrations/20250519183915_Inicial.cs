66ffade1306fabe20aa32ea2ffc41532fcaad246
danieldumar <daniel.dumarques@gamil.com>
2025-05-19 15:49:08 -0300

    Adicionar arquivos de projeto.

---
+++ Migrations/20250519183915_Inicial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanceCertoLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Leiloes",
+                columns: table => new
+                {
+                    LeilaoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataFim = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leiloes", x => x.LeilaoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Leiloes");
+        }
+    }
+}
---
+++ Models/Leilao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LanceCertoLite.Models
+{
+    public class Leilao
+    {
+        public int LeilaoId { get; set; }
+
+        [Required]
+        public string Titulo { get; set; }
+
+        public DateTime DataInicio { get; set; }
+
+        public DateTime DataFim { get; set; }
+    }
+}
